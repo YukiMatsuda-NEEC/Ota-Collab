d14f1e639134402950a76aec45579bcc55df0c5a
arai1444 <k021c1444@g.neec.ac.jp>
2022-04-28 15:24:06 +0900

    feat: タイトルコンポーネントの追加

---
+++ components/Title.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p v-if="isUnderline" class="underline">
+      <slot />
+    </p>
+    <p v-else>
+      <slot />
+    </p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  color: black;
+  font-size: 12px;
+  font-weight: 700;
+}
+.underline {
+  text-decoration: underline;
+}
+</style>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    isUnderline: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
