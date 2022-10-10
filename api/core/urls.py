cf3ea317fd3e7ba6ed1e58b0ed457b80235056bd
suzukitttt <k021c1031@g.neec.ac.jp>
2022-10-10 21:24:54 +0900

    Djangoの導入、axiosでフロントとバックのデータのやり取り

---
+++ api/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import matching
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    # matching/<slug>にアクセスしたらviewsのmatching関数を呼ぶ
+    path("matching/<slug>", views.matching, name='matching'),
+]
