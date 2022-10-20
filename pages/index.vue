035e3f651cb64217aa1076dab19af591baef1a41
HoshiKouichirou <kou555.hoshi@gmail.com>
2022-10-20 11:36:43 +0900

    feat: add firebase and fix edit profile ui

--- pages/index.vue
+++ pages/index.vue
@@ -68,9 +68,44 @@ export default {
       userName: "田中",
       isInputMode: false,
       isEditing: false,
+      profileData: {
+        Facebook: "海苔専門店 川島屋",
+        Instagram: "kawashimaya.nori",
+        LINE管理者: "松山聞義",
+        "LINE管理者（フリガナ）": "マツヤマブンギ",
+        Twitter: "kawashimayanori",
+        お店の紹介文:
+          "お中元やお歳暮の贈答用の海苔を中心に扱っております。製造コストを最小限に抑え最大限品質を維持するため、今まで製造から販売、出荷までの全行程を自社で行っていたものをイチから見直し、部分的に外注を取り入れ効率化を図り、店頭販売、電話注文をやめオンラインショップの通販のみの販売に特化しました。コロナはもちろん、目まぐるしく変わる社会環境に合わせて最適な運用を目指し日々取り組んでおります。",
+        コラボ願望:
+          "商品製造過程でどうしても正規製品にならない原料がでてしまいます。品質的に全く問題ないのですが、多少欠けていたり割れてしまったり、海苔という繊細な食材を扱う上で避けられない問題です。こういった原料を上手に活用していただける飲食店さんに是非提供したいです。",
+        代表者名: "松山聞義",
+        店舗住所: "東京都大田区大森本町2-3-3",
+        店舗名: "株式会社川島屋",
+        業種: "海苔販売業",
+      },
     };
   },
+  created() {
+    this.firebase();
+  },
   methods: {
+    async firebase() {
+      // let docRef = this.$fire.firestore.collection("users").doc("1"); //プロフィール取得
+      // console.log(docRef);
+      //   docRef
+      //     .get()
+      //     .then((doc) => {
+      //       if (doc.exists) {
+      //         console.log("Document data:", doc.data());
+      //       } else {
+      //         // doc.data() will be undefined in this case
+      //         console.log("No such document!");
+      //       }
+      //     })
+      //     .catch((error) => {
+      //       console.log("Error getting document:", error);
+      //     });
+    },
     Edit() {
       this.isEditing = !this.isEditing;
     },
