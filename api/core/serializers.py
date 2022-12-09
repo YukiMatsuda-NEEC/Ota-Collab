ff14f0c4bc62d413d52c5a1a7d4760bd2b94db79
YukiMatsuda-NEEC <102204127+YukiMatsuda-NEEC@users.noreply.github.com>
2022-12-09 15:09:41 +0900

    Delete api directory

--- api/core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class OffersSerializer(serializers.Serializer):
-    offers = serializers.ListField(
-        child = serializers.IntegerField()
-    )
-    
-class LastNumSerializer(serializers.Serializer):
-    lastNum = serializers.CharField()
