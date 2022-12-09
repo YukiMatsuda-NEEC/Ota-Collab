7e5002f9939af221edbe9787450da87887e0aab8
HoshiKouichirou <kou555.hoshi@gmail.com>
2022-12-09 10:50:19 +0900

    add: qrcode

--- pages/offerProfile.vue
+++ pages/offerProfile.vue
@@ -4,8 +4,16 @@
       <p>OTA collab.</p>
     </header>
     <section class="header-imgs">
-      <img src="~/assets/image/sample-image/kawashimaHeader.jpg" alt="ヘッダーイメージ" class="header-img" />
-      <img src="~/assets/image/sample-image/kawashimaIcon.jpg" alt="ヘッダーアイコン" class="header-icon" />
+      <img
+        src="~/assets/image/sample-image/kawashimaHeader.jpg"
+        alt="ヘッダーイメージ"
+        class="header-img"
+      />
+      <img
+        src="~/assets/image/sample-image/kawashimaIcon.jpg"
+        alt="ヘッダーアイコン"
+        class="header-icon"
+      />
       <p class="shop-name">{{ shop_name }}</p>
     </section>
     <section class="profiles">
@@ -49,63 +57,171 @@
       <div class="profile">
         <Title> 経営課題 </Title>
         <div class="profile-issues">
-          <input id="1" class="issue-checkbox" type="checkbox" value="1" v-model="issues" disabled />
+          <input
+            id="1"
+            class="issue-checkbox"
+            type="checkbox"
+            value="1"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="1">集客</label>
-          <input id="2" class="issue-checkbox" type="checkbox" value="2" v-model="issues" disabled />
+          <input
+            id="2"
+            class="issue-checkbox"
+            type="checkbox"
+            value="2"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="2">認知度</label>
-          <input id="3" class="issue-checkbox" type="checkbox" value="3" v-model="issues" disabled />
+          <input
+            id="3"
+            class="issue-checkbox"
+            type="checkbox"
+            value="3"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="3">ブランディング</label>
-          <input id="4" class="issue-checkbox" type="checkbox" value="4" v-model="issues" disabled />
+          <input
+            id="4"
+            class="issue-checkbox"
+            type="checkbox"
+            value="4"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="4">社員教育</label>
-          <input id="5" class="issue-checkbox" type="checkbox" value="5" v-model="issues" disabled />
+          <input
+            id="5"
+            class="issue-checkbox"
+            type="checkbox"
+            value="5"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="5">販路拡大</label>
-          <input id="6" class="issue-checkbox" type="checkbox" value="6" v-model="issues" disabled />
+          <input
+            id="6"
+            class="issue-checkbox"
+            type="checkbox"
+            value="6"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="6">来店頻度</label>
-          <input id="7" class="issue-checkbox" type="checkbox" value="7" v-model="issues" disabled />
+          <input
+            id="7"
+            class="issue-checkbox"
+            type="checkbox"
+            value="7"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="7">人材確保</label>
-          <input id="8" class="issue-checkbox" type="checkbox" value="8" v-model="issues" disabled />
+          <input
+            id="8"
+            class="issue-checkbox"
+            type="checkbox"
+            value="8"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="8">新規顧客</label>
-          <input id="9" class="issue-checkbox" type="checkbox" value="9" v-model="issues" disabled />
+          <input
+            id="9"
+            class="issue-checkbox"
+            type="checkbox"
+            value="9"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="9">流失客</label>
-          <input id="10" class="issue-checkbox" type="checkbox" value="10" v-model="issues" disabled />
+          <input
+            id="10"
+            class="issue-checkbox"
+            type="checkbox"
+            value="10"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="10">購入点数</label>
-          <input id="11" class="issue-checkbox" type="checkbox" value="11" v-model="issues" disabled />
+          <input
+            id="11"
+            class="issue-checkbox"
+            type="checkbox"
+            value="11"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="11">リピート率</label>
-          <input id="12" class="issue-checkbox" type="checkbox" value="12" v-model="issues" disabled />
+          <input
+            id="12"
+            class="issue-checkbox"
+            type="checkbox"
+            value="12"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="12">売上</label>
-          <input id="13" class="issue-checkbox" type="checkbox" value="13" v-model="issues" disabled />
+          <input
+            id="13"
+            class="issue-checkbox"
+            type="checkbox"
+            value="13"
+            v-model="issues"
+            disabled
+          />
           <label class="issue" for="13">商品単価</label>
         </div>
       </div>
 
       <div class="profile" v-show="show">
         <Title> LINE QRコード </Title>
-        <img src="~/assets/image/sample-image/my_qrcode_1665625823793.jpg" height="155px" weight="155px" />
+        <img src="QrUrl" height="155px" weight="155px" />
       </div>
 
       <!-- オファー箱 -->
-      <section v-if="($route.params.displayType == 1)" v-show="!show" class="profile">
-        <ota-Button @click="interestOffer" buttonStyle="interest"> 興味ある </ota-Button>
+      <section
+        v-if="$route.params.displayType == 1"
+        v-show="!show"
+        class="profile"
+      >
+        <ota-Button @click="interestOffer" buttonStyle="interest">
+          興味ある
+        </ota-Button>
         <ota-Button @click="passOffer" buttonStyle="pass"> パス </ota-Button>
       </section>
       <!-- オススメ -->
-      <section v-if="($route.params.displayType == 2)" class="profile">
-        <ota-Button @click="submitOffer" buttonStyle="offer"> オファー送信 </ota-Button>
+      <section v-if="$route.params.displayType == 2" class="profile">
+        <ota-Button @click="submitOffer" buttonStyle="offer">
+          オファー送信
+        </ota-Button>
       </section>
       <!-- 共通 -->
       <section class="profile">
-        <ota-Button @click="returnBeforePage" buttonStyle="return">一覧に戻る</ota-Button>
+        <ota-Button @click="returnBeforePage" buttonStyle="return"
+          >一覧に戻る</ota-Button
+        >
       </section>
-
     </section>
   </div>
 </template>
 
-
 <script>
 import Title from "~/components/Title.vue";
-import { getFirestore, doc, collection, getDoc, addDoc, serverTimestamp, updateDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  collection,
+  getDoc,
+  addDoc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 
 export default {
   name: "IndexPage",
@@ -126,7 +242,8 @@ export default {
       line_administrator: "",
       line_furigana: "",
       introduction: "",
-      issues: []
+      QrUrl: "",
+      issues: [],
     };
   },
   mounted() {
@@ -136,9 +253,11 @@ export default {
     // オファーから移動してきたときのデータの取得
     async getData() {
       const db = getFirestore();
-      const docSnapProfile = await getDoc(doc(db, "users", this.$route.params.userNum));
+      const docSnapProfile = await getDoc(
+        doc(db, "users", this.$route.params.userNum)
+      );
       if (docSnapProfile.exists()) {
-        const user = docSnapProfile.data();  // ユーザ情報の取得
+        const user = docSnapProfile.data(); // ユーザ情報の取得
         this.message = user.message;
         this.shop_name = user.shop_name;
         this.representative = user.representative;
@@ -150,22 +269,72 @@ export default {
       } else {
         console.log("No such document.");
       }
-      const docSnapIssues = await getDoc(doc(db, "ManagementIssues", this.$route.params.userNum));
+      const storage = getStorage();
+      const listRef = ref(storage, "/" + this.userNum); //ユーザーイメージの取得
+      console.log("test", this.userNum);
+      listAll(listRef).then((res) => {
+        console.log(res.items);
+        for (var i = 0; res.items.length > i; i++) {
+          console.log(res.items[i].name);
+          if (res.items[i].name.split(".")[0] == "QR") {
+            const pathReference = ref(
+              storage,
+              this.userNum + "/" + res.items[i].name
+            );
+            getDownloadURL(ref(storage, this.userNum + "/" + res.items[i].name))
+              .then((url) => {
+                this.QrUrl = url;
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }
+        }
+      });
+      const docSnapIssues = await getDoc(
+        doc(db, "ManagementIssues", this.$route.params.userNum)
+      );
       if (docSnapIssues.exists()) {
-        const issuesData = docSnapIssues.data();  // ユーザの経営課題の取得
-        if (issuesData.attracting_customers) { this.issues.push("1") };
-        if (issuesData.awareness) { this.issues.push("2") };
-        if (issuesData.branding) { this.issues.push("3") };
-        if (issuesData.employee_training) { this.issues.push("4") };
-        if (issuesData.expansion) { this.issues.push("5") };
-        if (issuesData.frequency) { this.issues.push("6") };
-        if (issuesData.human_resources) { this.issues.push("7") };
-        if (issuesData.new_customers) { this.issues.push("8") };
-        if (issuesData.outflow) { this.issues.push("9") };
-        if (issuesData.purchases) { this.issues.push("10") };
-        if (issuesData.repeat_rate) { this.issues.push("11") };
-        if (issuesData.sales) { this.issues.push("12") };
-        if (issuesData.unit_price) { this.issues.push("13") };
+        const issuesData = docSnapIssues.data(); // ユーザの経営課題の取得
+        if (issuesData.attracting_customers) {
+          this.issues.push("1");
+        }
+        if (issuesData.awareness) {
+          this.issues.push("2");
+        }
+        if (issuesData.branding) {
+          this.issues.push("3");
+        }
+        if (issuesData.employee_training) {
+          this.issues.push("4");
+        }
+        if (issuesData.expansion) {
+          this.issues.push("5");
+        }
+        if (issuesData.frequency) {
+          this.issues.push("6");
+        }
+        if (issuesData.human_resources) {
+          this.issues.push("7");
+        }
+        if (issuesData.new_customers) {
+          this.issues.push("8");
+        }
+        if (issuesData.outflow) {
+          this.issues.push("9");
+        }
+        if (issuesData.purchases) {
+          this.issues.push("10");
+        }
+        if (issuesData.repeat_rate) {
+          this.issues.push("11");
+        }
+        if (issuesData.sales) {
+          this.issues.push("12");
+        }
+        if (issuesData.unit_price) {
+          this.issues.push("13");
+        }
       } else {
         console.log("No such document.");
       }
@@ -188,15 +357,15 @@ export default {
     },
     // おすすめからオファーを出す
     submitOffer() {
-      let uid = "";   // テスト中(初期値決める)
+      let uid = ""; // テスト中(初期値決める)
       const auth = getAuth();
       onAuthStateChanged(auth, async (user) => {
         if (user) {
-          uid = user.uid;  // ユーザのuid取得
+          uid = user.uid; // ユーザのuid取得
           const db = getFirestore();
           const docSnap = await getDoc(doc(db, "uid_to_num", uid));
           if (docSnap.exists()) {
-            this.userNum = docSnap.data().num;  // ユーザの連番取得
+            this.userNum = docSnap.data().num; // ユーザの連番取得
           } else {
             console.log("No such document.");
           }
@@ -214,12 +383,15 @@ export default {
     },
     // 遷移前のタブを表示する
     returnBeforePage() {
-      this.$router.push({ name: 'offer', params: { displayType: this.$route.params.displayType } });
+      this.$router.push({
+        name: "offer",
+        params: { displayType: this.$route.params.displayType },
+      });
     },
   },
 };
 </script>
-  
+
 <style lang="scss" scoped>
 textarea {
   resize: vertical;
@@ -271,7 +443,7 @@ textarea {
         display: none;
       }
 
-      input:checked+label {
+      input:checked + label {
         background-color: #f1da8a;
       }
 
